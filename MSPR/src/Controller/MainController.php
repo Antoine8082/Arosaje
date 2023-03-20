11ebeaaec99192a5f5b5ff75e7f9fb846c653f93
Curzeed <xavier.lecapitaine4450@gmail.com>
2023-03-20 15:37:00 +0100

    Pushing objects

--- MSPR/src/Controller/MainController.php
+++ MSPR/src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $pr = $em->getRepository(Post::class);
         return $this->render('main/index.html.twig', [
-
+            'posts' => $pr->findAll()
         ]);
     }
 }

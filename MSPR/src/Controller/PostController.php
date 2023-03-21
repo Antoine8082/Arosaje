c1276a424f8c8d2229b7673a7bdcf49265e724af
simonhuchede <simonhuchede@gmail.com>
2023-03-21 18:12:41 +0100

    -feat : move post form to another twig

--- MSPR/src/Controller/PostController.php
+++ MSPR/src/Controller/PostController.php
@@ -12,12 +12,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     #[Route('/posts/{id}', name: 'app_post_detail')]
-    public function detail(Post $post,Request $request): Response
+    public function detail(Post $post): Response
     {
-        $currentRoute = $request->get('_route');
         return $this->render('post/detail.html.twig', [
             'post' => $post,
-            'route'=> $currentRoute
         ]);
+    }
+    #[Route('/newpost', name: 'app_new_post')]
+    public function addPost(EntityManagerInterface $em,Request $request): Response
+    {
+        if($request->getMethod() == "POST"){
+        $post = new Post();
+        $uploadFile = $request->files->get('img');
+        $originalFileName = pathinfo($uploadFile->getClientOriginalName(),PATHINFO_FILENAME);
+        $safeFileName = preg_replace('/[^a-zA-Z0-9]/','_',$originalFileName);
+        $newFileName = $safeFileName . '-'. uniqid() . '.' . $uploadFile->guessExtension();
+        $uploadFile->move($this->getParameter('images_directory'),$newFileName);
+        $post->setDescription($_POST['description']);
+        $post->setTitle($_POST['title']);
+        $post->setUserPost($this->getUser());
+        $post->setImage($newFileName);
+        $em->persist($post);
+        $em->flush();
+        $this->redirectToRoute('app_main');
+    }
+
+        return $this->render('post/form.html.twig');
+
     }
 }

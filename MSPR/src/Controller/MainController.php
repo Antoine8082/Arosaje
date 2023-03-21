c4776a49555738f784d1ed56b7386685f54bbf82
Curzeed <xavier.lecapitaine4450@gmail.com>
2023-03-21 10:33:51 +0100

    form post

--- MSPR/src/Controller/MainController.php
+++ MSPR/src/Controller/MainController.php
@@ -5,33 +5,40 @@ namespace App\Controller;
 use App\Entity\Post;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(EntityManagerInterface $em): Response
+    public function index(EntityManagerInterface $em, Request $request): Response
     {
-        $isMain = true;
-        $isProfile = false;
+        if($this->getUser() == null){
+            return $this->redirectToRoute('app_login');
+        }
+        if($request->getMethod() == "POST"){
+            $post = new Post();
+            $post->setDescription($_POST['description']);
+            $post->setTitle($_POST['title']);
+            $post->setUserPost($this->getUser());
+            $em->persist($post);
+            $em->flush();
+        }
         $pr = $em->getRepository(Post::class);
         return $this->render('main/index.html.twig', [
             'posts' => $pr->findAll(),
-            'isProfile'=>$isProfile,
-            'isMain'=>$isMain
         ]);
     }
 
     #[Route('/profile', name: 'app_profile')]
     public function profile(EntityManagerInterface $em): Response
     {
-        $isMain = false;
-        $isProfilePage = true;
         return $this->render('profile/index.html.twig',['posts'=> $this->getUser()->getPost(),
             'guardedPosts'=>$this->getUser()->getGuardedPosts(),
-            'isProfile'=> $isProfilePage,
-            'isMain'=>$isMain]);
+            ]
+        );
     }
 
 }

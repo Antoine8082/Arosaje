f9a7c5e201385fa14657e6066da9d3b780a2cfd0
simonhuchede <simonhuchede@gmail.com>
2023-03-23 09:22:44 +0100

    -feat : add register redirect

--- MSPR/src/Controller/MainController.php
+++ MSPR/src/Controller/MainController.php
@@ -18,20 +18,6 @@ class MainController extends AbstractController
         if($this->getUser() == null){
             return $this->redirectToRoute('app_login');
         }
-        if($request->getMethod() == "POST"){
-            $post = new Post();
-            $uploadFile = $request->files->get('img');
-            $originalFileName = pathinfo($uploadFile->getClientOriginalName(),PATHINFO_FILENAME);
-            $safeFileName = preg_replace('/[^a-zA-Z0-9]/','_',$originalFileName);
-            $newFileName = $safeFileName . '-'. uniqid() . '.' . $uploadFile->guessExtension();
-            $uploadFile->move($this->getParameter('images_directory'),$newFileName);
-            $post->setDescription($_POST['description']);
-            $post->setTitle($_POST['title']);
-            $post->setUserPost($this->getUser());
-            $post->setImage($newFileName);
-            $em->persist($post);
-            $em->flush();
-        }
         $pr = $em->getRepository(Post::class);
         return $this->render('main/index.html.twig', [
             'posts' => $pr->findAll()
